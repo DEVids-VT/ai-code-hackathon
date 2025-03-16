2b17bf585e6e609b10114eed3bde5d30a21186a0
Stefan Petrov <106827257+stefan-petrov1@users.noreply.github.com>
2025-03-16 14:27:01 +0200

    roles

--- src/hooks/useUserCredentials.ts
+++ src/hooks/useUserCredentials.ts
@@ -1,10 +1,12 @@
-import { IUser } from '@/types';
+import { IUser, UserRole } from '@/types';
 import { useAuth0 } from '@auth0/auth0-react';
 
 export interface IUseUserCredentialsReturnData {
   user: IUser;
   isAuthenticated: boolean;
 }
+
+const DOMAIN = 'https://hoteachclaims.com';
 
 export const useUserCredentials = (): IUseUserCredentialsReturnData => {
   const { user, isAuthenticated } = useAuth0();
@@ -15,11 +17,7 @@ export const useUserCredentials = (): IUseUserCredentialsReturnData => {
       name: user?.name as string,
       email: user?.email as string,
       picture: user?.picture as string,
-      // citizenshipCountry: user?.[`${DOMAIN}/citizenship_country`] as string,
-      // dateOfBirth: user?.[`${DOMAIN}/date_of_birth`] as string,
-      // fullName: user?.[`${DOMAIN}/full_name`] as string,
-      // phoneNumber: user?.[`${DOMAIN}/phone_number`] as string,
-      // roles: user?.[`${DOMAIN}/roles`] as UserRole[],
+      roles: user?.[`${DOMAIN}/roles`] as UserRole[],
     },
     isAuthenticated,
   };
